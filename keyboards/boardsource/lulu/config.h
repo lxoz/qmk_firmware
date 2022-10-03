32c102606d01e9a9a6c747a8ccb15e1f3b43011d
lxoz <whois.alex.oguz@gmail.com>
2022-10-03 12:07:45 +0200

    More tweaks

--- keyboards/boardsource/lulu/config.h
+++ keyboards/boardsource/lulu/config.h
@@ -11,3 +11,16 @@
 #define ENABLE_RGB_MATRIX_BREATHING
 #define ENABLE_RGB_MATRIX_BAND_SAT
 #define ENABLE_RGB_MATRIX_BAND_VAL
+
+
+
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Prevent normal rollover on alphas from accidentally triggering mods.
+#define IGNORE_MOD_TAP_INTERRUPT
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+
+#define UNICODE_SELECTED_MODES UC_MAC, UC_LNX, UC_WINC
